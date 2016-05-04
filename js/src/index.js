60aa79ead12ff7a76bf72a3e7f88ffcee0fe7824
wangjiankui <952035865@qq.com>
2016-05-04 17:43:27 +0800

    完成关联图书页

--- js/src/index.js
+++ js/src/index.js
@@ -1,10 +1,34 @@
 /**
  * Created by Administrator on 2016/4/28 0028.
  */
-function abc (){
-	var a = true;
-	if (a){
+var $navbarOut = $(".navbar_out");
+$navbarOut.find('.navbar_con').hide();
+$navbarOut.find('h3').click(function () {
+	var $tipsThis = $(".tips_icon",this);
+	var next = $(this).nextAll();
+	$tipsThis.toggleClass("tips_active");
+	next.slideToggle('fast');
+	$('.navbar_con').not(next).slideUp('fast');
+	$(".bd_img").find(".tips_icon").not($tipsThis).removeClass("tips_active");
+	return false;
+});
 
-		console.log("我是测试")
-	}
-}
+$(".result-list").hover(function(){
+	$(this).toggleClass("result-bd");
+});
+
+$(".link-del").click(function(){
+	$(".del_alert").fadeIn();
+});
+
+$(".add_user").click(function(){
+	$(".add_user_alert").fadeIn();
+});
+
+$(".del").click(function(){
+	$(".add_user_alert").fadeOut();
+});
+
+$(".cencle").click(function(){
+	$(".del_alert").fadeOut();
+})
